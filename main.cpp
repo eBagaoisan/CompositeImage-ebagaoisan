9349577fcdbbd1d9895f2f1f9fc45f9f30149501
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-11-06 11:02:45 -0800

    Updated bitmap files.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,34 @@
 
 using namespace std;
 
+//Prototypes
+void validImage(Bitmap image);
+void getImage(Bitmap input);
+void sizeImage(Bitmap size);
+
 int main()
 {
 
     
+
+}
+
+//checks if user input is a valid image
+void validImage(Bitmap image){
+    image.open(file);
+    bool realImage = image.isImage();
+    if (realImage == false){
+        
+    }
+    
+}
+
+//asks user for image
+void getImage(Bitmap input){
+
+}
+
+//checks if new image is the same size as previous image
+void sizeImage(Bitmap size){
 
 }
