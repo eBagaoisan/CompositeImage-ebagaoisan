77770a5864e5d3f721e07f5ab2f781a92bf1ba11
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-11-12 15:56:18 -0800

    Added functions of programs purpose and comments explaining each function.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,29 @@
 #include "bitmap.h"
 #include <iostream>
-#include <string>
+#include <vector>
+#include <stream>
 
 using namespace std;
 
 //Prototypes
-void validImage(Bitmap image);
-void getImage(Bitmap input);
-void sizeImage(Bitmap size);
+void validBMP(Bitmap);
+void sizeImage(Bitmap);
+vector< vector<Pixel> > convertBMP(Bitmap &);
+vector< vector<Pixel> > greyScale(vector<vector<Pixel> >&);
+vector< vector<pixel> > composite(vector<vector<Pixeel> >&);
 
 int main()
 {
 
     
-
+    return 0;
 }
 
-//checks if user input is a valid image
-void validImage(Bitmap image){
-    image.open(file);
+/*Takes users image input and checks if user input is a valid BMP image, 
+if not then asks user to access another file that
+is valid.*/
+bool validBMP(Bitmap bmp){
+    image.open(bmp);
     bool realImage = image.isImage();
     if (realImage == false){
         
@@ -26,12 +31,25 @@ void validImage(Bitmap image){
     
 }
 
-//asks user for image
-void getImage(Bitmap input){
+//Converts valid image into a pixel matrix
+vector< vector<Pixel> > convertBMP(Bitmap & image){
+//toPixelmatrix
+}
+
+/*checks if new image is the same size as previous image
+by checking and comparing pixel size of both images,
+if invalid return to main function loop
+*/
+bool sizeImage(Bitmap size){
 
 }
 
-//checks if new image is the same size as previous image
-void sizeImage(Bitmap size){
+//Takes matrix of pixels and takes average values of all RGB components
+vector<vector<Pixel> > greyScale(vector<vector<Pixel> >& imgMatrix){
+
+}
+
+//Takes values of matrices and gets the averages of all 10 images and processes each image into one matrix
+vector<vector<Pixel> > composite(vector<vector<Pixel> >& matrix){
 
 }
